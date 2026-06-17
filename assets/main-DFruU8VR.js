95af2ac099a295c56c4e09a12fc317fb045f13c4
jens <jens@claude>
2026-06-17 07:20:19 +0000

    deploy: Baujahr 1965

---
+++ assets/main-DFruU8VR.js
@@ -0,0 +1,1 @@
+import{c as a,r as e,_ as r}from"./reveal-ByzML0vM.js";a(r).directive("reveal",e).mount("#app");
--- assets/main-sg2hWuOG.js
+++
@@ -1,1 +0,0 @@
-import{c as a,r as e,_ as r}from"./reveal-UiGiHCBa.js";a(r).directive("reveal",e).mount("#app");
